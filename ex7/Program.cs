d7f7589db31d50804447fbcc9db70f050930615c
Christian Flinker Sandbeck <chfs@via.dk>
2018-10-08 15:38:08 +0200

    a solution to exercise 7

---
+++ ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex7
+{
+    class Program
+    {
+        // declaring the delegate
+        public delegate void Notifier(string name);
+
+        static void Main(string[] args)
+        {
+            // creating a variable to hold the delegate
+            Notifier caller = sayHello;
+            caller += sayGoodbye;
+
+            // using the delegate
+            caller("Albert");
+        }
+
+        // creating methods compatible with the Notifier delegate
+        public static void sayHello(string name)
+        {
+            System.Console.WriteLine( $"Hello {name}" );
+        }
+
+        public static void sayGoodbye(string name)
+        {
+            System.Console.WriteLine( $"Goodbye {name}" );
+        }
+    }
+}
